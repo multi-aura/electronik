0018996b0af2716b9a523547eeaeb15a24d3bb0d
hoanggum <nguyenhuyhoang20032020@gmail.com>
2024-12-13 21:57:36 +0700

    feature/my_order

--- frontend-web/src/components/Logo/Logo.js
+++ frontend-web/src/components/Logo/Logo.js
@@ -1,12 +1,15 @@
 // src/components/Logo/Logo.js
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../assets/img/Logo_black.png';
 
 const Logo = () => {    
   return (
     <div className="logo">
-      <img src={logo} alt="Logo" style={{ height: '70px' }} />
-    </div>
+      <Link to="/">
+        <img src={logo} alt="Logo" style={{ height: '70px' }} />
+      </Link>
+  </div>
   );
 };
 
--- frontend-web/src/components/Sidebar/Sidebar.js
+++ frontend-web/src/components/Sidebar/Sidebar.js
@@ -1,6 +1,7 @@
 // src/components/Sidebar/Sidebar.js
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
 import { faLaptop, faDesktop, faMemory, faKeyboard, faMouse, faHeadphones, faGamepad } from '@fortawesome/free-solid-svg-icons';
 import './Sidebar.css';
 
@@ -12,10 +13,10 @@ const Sidebar = ({ categories }) => {
       <ul className="list-unstyled">
         {categories.map((category, index) => (
           <li key={index} className="category-item">
-            <div className="d-flex align-items-center category-header">
+             <Link to={category.link} className="d-flex align-items-center category-header">
               <FontAwesomeIcon icon={icons[index % icons.length]} className="sidebar-icon me-2" />
               <span>{category.label}</span>
-            </div>
+            </Link>
             <ul className="submenu list-unstyled">
               {category.subcategories.map((subcategory, subIndex) => (
                 <li key={subIndex} className="submenu-item">
